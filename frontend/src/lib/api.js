5cb2bf8c5d28f33c3e04769dfb9d3f3b43be2d86
ellyg <ellygatsi@gmail.com>
2026-06-28 18:38:02 -0400

    fix duplicate export

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -5,8 +5,6 @@ const api = axios.create({
   withCredentials: true,
 });
 
-export default api;
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
